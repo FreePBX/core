6e9ffa34ff14da69031c4cc248ccba7c9e2486fb
Rob Thomas <xrobau@gmail.com>
2006-11-27 02:42:49 +0000

    Add Greg's redirect() call to fix #1335, SQL error when adding administrators.

--- page.ampusers.php
+++ page.ampusers.php
@@ -72,18 +72,21 @@ switch ($action) {
 		core_ampusers_add($username, $password, $extension_low, $extension_high, $deptname, $sections);
 		//indicate 'need reload' link in footer.php 
 		needreload();
+		redirect('config.php?type=setup&display=ampusers');
 	break;
 	case "editampuser":
 		core_ampusers_del($userdisplay);
 		core_ampusers_add($username, $password, $extension_low, $extension_high, $deptname, $sections);
 		//indicate 'need reload' link in footer.php 
 		needreload();
+		redirect('config.php?type=setup&display=ampusers');
 	break;
 	case "delampuser":
 		core_ampusers_del($userdisplay);
 		//indicate 'need reload' link in footer.php 
 		needreload();
 		$userdisplay = ""; // go "add" screen
+		redirect('config.php?type=setup&display=ampusers');
 	break;
 }
 
