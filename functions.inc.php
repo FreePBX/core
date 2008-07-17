88f771743936f5846c228ea6a8a2137dc481aedc
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2008-07-17 04:10:51 +0000

    #2671 move macro-confirm to auto-generate, change to use background to handle compound recordings

--- functions.inc.php
+++ functions.inc.php
@@ -1199,6 +1199,77 @@ function core_get_config($engine) {
 				$ext->add($context, $exten, '', new ext_hangup());
 			}
 
+			/*
+			;------------------------------------------------------------------------
+			; [macro-confirm]
+			;------------------------------------------------------------------------
+			; CONTEXT:      macro-confirm                                                                                                              
+			; PURPOSE:      added default message if none supplied
+			;
+			; Follom-Me and Ringgroups provide an option to supply a message to be
+			; played as part of the confirmation. These changes have added a default
+			; message if none is supplied.
+			;
+			;------------------------------------------------------------------------
+			*/
+			$context = 'macro-confirm';
+			$exten = 's';
+
+			$ext->add($context, $exten, '', new ext_setvar('LOOPCOUNT','0'));
+			$ext->add($context, $exten, '', new ext_setvar('__MACRO_RESULT','ABORT'));
+			$ext->add($context, $exten, '', new ext_setvar('MSG1','${IF($["foo${ARG1}" != "foo"]?${ARG1}:"incoming-call-1-accept-2-decline")}'));
+			if (version_compare($version, "1.4", "ge")) { 
+				$ext->add($context, $exten, 'start', new ext_background('${MSG1},m,${CHANNEL(language)},macro-confirm'));
+			} else {
+				$ext->add($context, $exten, 'start', new ext_background('${MSG1},m,${LANGUAGE},macro-confirm'));
+			}
+			$ext->add($context, $exten, '', new ext_read('INPUT', '', 1, '', '', 4));
+			$ext->add($context, $exten, '', new ext_gotoif('$[${LEN(${INPUT})} > 0]', '${INPUT},1', 't,1'));
+
+			$exten = '1';
+			$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}" = "0"]', 'toolate,1'));
+			$ext->add($context, $exten, '', new ext_dbdel('RG/${ARG3}/${UNIQCHAN}'));
+			$ext->add($context, $exten, '', new ext_dbdel('${BLKVM_OVERRIDE}'));
+			$ext->add($context, $exten, '', new ext_setvar('__MACRO_RESULT',''));
+			$ext->add($context, $exten, '', new ext_macroexit());
+
+			$exten = '2';
+			$ext->add($context, $exten, '', new ext_goto(1, 'noanswer'));
+
+			$exten = '3';
+			$ext->add($context, $exten, '', new ext_saydigits('${CALLCONFIRMCID}'));
+			$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}" = "0"]', 'toolate,1','s,start'));
+
+			$exten = 't';
+			$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}" = "0"]', 'toolate,1'));
+			$ext->add($context, $exten, '', new ext_setvar('LOOPCOUNT','$[ ${LOOPCOUNT} + 1 ]'));
+			$ext->add($context, $exten, '', new ext_gotoif('$[ ${LOOPCOUNT} < 5 ]', 's,start','noanswer,1'));
+
+			$exten = '_X';
+			if (version_compare($version, "1.4", "ge")) { 
+				$ext->add($context, $exten, '', new ext_background('invalid,m,${CHANNEL(language)},macro-confirm'));
+			} else {
+				$ext->add($context, $exten, '', new ext_background('invalid,m,${LANGUAGE},macro-confirm'));
+			}
+			$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}" = "0"]', 'toolate,1'));
+			$ext->add($context, $exten, '', new ext_setvar('LOOPCOUNT','$[ ${LOOPCOUNT} + 1 ]'));
+			$ext->add($context, $exten, '', new ext_gotoif('$[ ${LOOPCOUNT} < 5 ]', 's,start','noanswer,1'));
+
+			$exten = 'noanswer';
+			$ext->add($context, $exten, '', new ext_setvar('__MACRO_RESULT','ABORT'));
+			$ext->add($context, $exten, '', new ext_macroexit());
+
+			$exten = 'toolate';
+			$ext->add($context, $exten, '', new ext_setvar('MSG2','${IF($["foo${ARG2}" != "foo"]?${ARG2}:"incoming-call-no-longer-avail")}'));
+			$ext->add($context, $exten, '', new ext_playback('${MSG2}'));
+			$ext->add($context, $exten, '', new ext_setvar('__MACRO_RESULT','ABORT'));
+			$ext->add($context, $exten, '', new ext_macroexit());
+
+			$exten = 'h';
+			$ext->add($context, $exten, '', new ext_macro('hangupcall'));
+
+
+
 			$context = 'macro-dialout-trunk';
 			$exten = 's';
 			
@@ -1551,7 +1622,11 @@ function core_get_config($engine) {
 			$exten = 'docheck';
 			
 			$ext->add($context, $exten, '', new ext_playback('beep'));
-			$ext->add($context, $exten, 'dc_start', new ext_background('${RECFILE},m,${CHANNEL(language)},macro-systemrecording'));
+			if (version_compare($version, "1.4", "ge")) { 
+				$ext->add($context, $exten, 'dc_start', new ext_background('${RECFILE},m,${CHANNEL(language)},macro-systemrecording'));
+			} else {
+				$ext->add($context, $exten, 'dc_start', new ext_background('${RECFILE},m,${LANGUAGE},macro-systemrecording'));
+			}
 			$ext->add($context, $exten, '', new ext_wait(1));
 			$ext->add($context, $exten, '', new ext_goto(1, 'confmenu'));
 
