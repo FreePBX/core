a18e85fd2a96d6f37c044958e815c40bf122fc1f
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2009-05-01 19:06:13 +0000

    closes #3649 redo trunks with proper trunk tables

--- install.php
+++ install.php
@@ -247,7 +247,7 @@ if(DB::IsError($check)) {
 // TODO: move this to the upgrade script so it only has to be done
 //       once in a major upgrade when we move to next major release
 //
-outn("Checking for Global var RECORDING_STATE..");
+outn(_("Checking for Global var RECORDING_STATE.."));
 $nrows = $db->getOne("SELECT count(*) from globals where variable='RECORDING_STATE'");
 if (!$nrows) {
 	$db->query("insert into globals values ('RECORDING_STATE', 'ENABLED')");
@@ -255,5 +255,295 @@ if (!$nrows) {
 } else {
 	out(_("Already exists!"));
 }
+
+
+// This next set of functions and code are used to migrate from the old
+// global variable storage of trunk data to the new trunk table and trunk
+// pattern table for localprefixes.conf
+//
+
+//Sort trunks for sqlite
+function __sort_trunks($a,$b)  {
+        global $unique_trunks;
+        ereg("OUT_([0-9]+)",$unique_trunks[$a][0],$trunk_num1);
+        ereg("OUT_([0-9]+)",$unique_trunks[$b][0],$trunk_num2);
+        return ($trunk_num1[1] >= $trunk_num2[1]? 1:-1);
+}
+
+// Get values from localprefix configuration file where values are stored
+// for fixlocalprefix macro
+//
+function __parse_DialRulesFile($filename, &$conf, &$section) {
+	if (is_null($conf)) {
+		$conf = array();
+	}
+	if (is_null($section)) {
+		$section = "general";
+	}
+	
+	if (file_exists($filename)) {
+		$fd = fopen($filename, "r");
+		while ($line = fgets($fd, 1024)) {
+			if (preg_match("/^\s*([a-zA-Z0-9-_]+)\s*=\s*(.*?)\s*([;#].*)?$/",$line,$matches)) {
+				// name = value
+				// option line
+				$conf[$section][ $matches[1] ] = $matches[2];
+			} else if (preg_match("/^\s*\[(.+)\]/",$line,$matches)) {
+				// section name
+				$section = strtolower($matches[1]);
+			} else if (preg_match("/^\s*#include\s+(.*)\s*([;#].*)?/",$line,$matches)) {
+				// include another file
+				
+				if ($matches[1][0] == "/") {
+					// absolute path
+					$filename = $matches[1];
+				} else {
+					// relative path
+					$filename =  dirname($filename)."/".$matches[1];
+				}
+				__parse_DialRulesFile($filename, $conf, $section);
+			}
+		}
+	}
+}
+
+function __migrate_trunks_to_table() {
+
+	global $db;
+	global $amp_conf;
+
+	$sql = "
+	CREATE TABLE `trunks` 
+	( 
+		`trunkid` INTEGER,
+		`name` VARCHAR( 50 ) NOT NULL DEFAULT '', 
+		`tech` VARCHAR( 20 ) NOT NULL , 
+		`outcid` VARCHAR( 40 ) NOT NULL DEFAULT '', 
+		`keepcid` VARCHAR( 4 ) DEFAULT 'off',
+		`maxchans` VARCHAR( 6 ) DEFAULT '',
+		`failscript` VARCHAR( 255 ) NOT NULL DEFAULT '', 
+		`dialoutprefix` VARCHAR( 255 ) NOT NULL DEFAULT '', 
+		`channelid` VARCHAR( 255 ) NOT NULL DEFAULT '', 
+		`usercontext` VARCHAR( 255 ) NULL, 
+		`provider` VARCHAR( 40 ) NULL, 
+		`disabled` VARCHAR( 4 ) DEFAULT 'off',
+	
+		PRIMARY KEY  (`trunkid`, `tech`, `channelid`) 
+	) 
+	";
+	$check = $db->query($sql);
+	if(DB::IsError($check)) {
+		if($check->getCode() == DB_ERROR_ALREADY_EXISTS) {
+			//echo ("already exists\n");
+			return false; 
+		} else {
+			die_freepbx($check->getDebugInfo());	
+		}
+	}
+	
+	// sqlite doesn't support the syntax required for the SQL so we have to do it the hard way
+	if ($amp_conf["AMPDBENGINE"] == "sqlite3") {
+		$sqlstr = "SELECT variable, value FROM globals WHERE variable LIKE 'OUT\_%' ESCAPE '\'";
+		$my_unique_trunks = sql($sqlstr,"getAll",DB_FETCHMODE_ASSOC);
+
+		$sqlstr = "SELECT variable, value FROM globals WHERE variable LIKE 'OUTDISABLE\_%' ESCAPE '\'";
+		$disable_states = sql($sqlstr,"getAll",DB_FETCHMODE_ASSOC);
+
+		foreach($disable_states as $arr)  {
+			$disable_states_assoc[$arr['variable']] = $arr['value'];
+		}
+		global $unique_trunks;
+		$unique_trunks = array();
+
+		foreach ($my_unique_trunks as $this_trunk) {
+
+			$trunk_num = substr($this_trunk['variable'],4);
+			$this_state = (isset($disable_states_assoc['OUTDISABLE_'.$trunk_num]) ? $disable_states_assoc['OUTDISABLE_'.$trunk_num] : 'off');
+			$unique_trunks[] = array($this_trunk['variable'], $this_trunk['value'], $this_state);
+		}
+		// sort this array using a custom function __sort_trunks(), defined above
+		uksort($unique_trunks,"__sort_trunks");
+		// re-index the newly sorted array
+		foreach($unique_trunks as $arr) {
+			$unique_trunks_t[] = array($arr[0],$arr[1],$arr[2]);
+		}
+		$unique_trunks = $unique_trunks_t;
+
+	} else {
+		$sqlstr  = "SELECT t.variable, t.value, d.value state FROM `globals` t ";
+		$sqlstr .= "JOIN (SELECT x.variable, x.value FROM globals x WHERE x.variable LIKE 'OUTDISABLE\_%') d ";
+		$sqlstr .= "ON substring(t.variable,5) = substring(d.variable,12) WHERE t.variable LIKE 'OUT\_%' ";
+		$sqlstr .= "UNION ALL ";
+		$sqlstr .= "SELECT v.variable, v.value, concat(substring(v.value,1,0),'off') state  FROM `globals` v ";
+		$sqlstr .= "WHERE v.variable LIKE 'OUT\_%' AND concat('OUTDISABLE_',substring(v.variable,5)) NOT IN ";
+		$sqlstr .= " ( SELECT variable from globals WHERE variable LIKE 'OUTDISABLE\_%' ) ";
+		$sqlstr .= "ORDER BY variable";
+		$unique_trunks = sql($sqlstr,"getAll");
+	}
+
+	$trunkinfo = array();
+	foreach ($unique_trunks as $trunk) {
+		list($tech,$name) = explode('/',$trunk[1]);
+		$trunkid = ltrim($trunk[0],'OUT_');
+
+		$sqlstr = "
+			SELECT `variable`, `value` FROM `globals` WHERE `variable` IN (
+				'OUTCID_$trunkid', 'OUTFAIL_$trunkid', 'OUTKEEPCID_$trunkid',
+				'OUTMAXCHANS_$trunkid', 'OUTPREFIX_$trunkid')
+		";
+		$trunk_attribs = sql($sqlstr,'getAll',DB_FETCHMODE_ASSOC);
+		$trunk_attrib_hash = array();
+		foreach ($trunk_attribs as $attribs) {
+			$trunk_attrib_hash[$attribs['variable']] = $attribs['value'];
+		}
+
+		switch ($tech) {
+			case 'SIP':
+				$tech = 'sip';
+				$user = sql("SELECT `data` FROM `sip` WHERE `id` = '99999$trunkid' AND `keyword` = 'account'",'getOne');
+				break;
+			case 'IAX':
+			case 'IAX2':
+				$tech = 'iax';
+				$user = sql("SELECT `data` FROM `iax` WHERE `id` = '99999$trunkid' AND `keyword` = 'account'",'getOne');
+				break;
+			case 'ZAP':
+			case 'DUNDI':
+			case 'ENUM':
+				$tech = strtolower($tech);
+				$user = '';
+				break;
+			default:
+				if (substr($tech,0,4) == 'AMP:') {
+					$tech='custom';
+					$name = substr($trunk[1],4);
+				} else {
+					$tech = strtolower($tech);
+				}
+				$user = '';
+		}
+
+		$trunkinfo[] = array(
+			'trunkid' =>       $trunkid,
+			'tech' =>          $tech,
+			'outcid' =>        $trunk_attrib_hash['OUTCID_'.$trunkid],
+			'keepcid' =>       $trunk_attrib_hash['OUTKEEPCID_'.$trunkid],
+			'maxchans' =>      $trunk_attrib_hash['OUTMAXCHANS_'.$trunkid],
+			'failscript' =>    $trunk_attrib_hash['OUTFAIL_'.$trunkid],
+			'dialoutprefix' => $trunk_attrib_hash['OUTPREFIX_'.$trunkid],
+			'channelid' =>     $name,
+			'usercontext' =>   $user,
+			'disabled' =>      $trunk[2], // disable state
+		);	
+
+		$sqlstr = "INSERT INTO `trunks` 
+			( trunkid, tech, outcid, keepcid, maxchans, failscript, dialoutprefix, channelid, usercontext, disabled) 
+			VALUES (
+				'".$db->escapeSimple($trunkid)."',
+				'".$db->escapeSimple($tech)."',
+				'".$db->escapeSimple($trunk_attrib_hash['OUTCID_'.$trunkid])."',
+				'".$db->escapeSimple($trunk_attrib_hash['OUTKEEPCID_'.$trunkid])."',
+				'".$db->escapeSimple($trunk_attrib_hash['OUTMAXCHANS_'.$trunkid])."',
+				'".$db->escapeSimple($trunk_attrib_hash['OUTFAIL_'.$trunkid])."',
+				'".$db->escapeSimple($trunk_attrib_hash['OUTPREFIX_'.$trunkid])."',
+				'".$db->escapeSimple($name)."',
+				'".$db->escapeSimple($user)."',
+				'".$db->escapeSimple($trunk[2])."'
+		  )
+		";
+		sql($sqlstr);
+	}
+
+	return $trunkinfo;
+}
+
+// __migrate_trunks_to_table will return false if the trunks table already exists and
+// no migration is needed
+//
+outn(_("Checking if trunk table migration required.."));
+$trunks = __migrate_trunks_to_table();
+if ($trunks !== false) {
+	outn(_("migrating.."));
+	foreach ($trunks as $trunk) {
+		$tech = $trunk['tech'];
+		$trunkid = $trunk['trunkid'];
+		switch ($tech) {
+			case 'sip':
+			case 'iax':
+				$sql = "UPDATE `$tech` SET `id` = 'tr-peer-$trunkid' WHERE `id` = '9999$trunkid'";
+				sql($sql);
+				$sql = "UPDATE `$tech` SET `id` = 'tr-user-$trunkid' WHERE `id` = '99999$trunkid'";
+				sql($sql);
+				$sql = "UPDATE `$tech` SET `id` = 'tr-reg-$trunkid' WHERE `id` = '9999999$trunkid' AND `keyword` = 'register'";
+				sql($sql);
+				break;
+			default:
+				break;
+		}
+	}
+	outn(_("removing globals.."));
+	// Don't do this above, in case something goes wrong
+	//
+	// At this point we have created our trunks table and update the sip and iax files
+	// time to get rid of the old globals which will not be auto-generated
+	//
+	foreach ($trunks as $trunk) {
+		$trunkid = $trunk['trunkid'];
+
+		$sqlstr = "
+			DELETE FROM `globals` WHERE `variable` IN (
+				'OUTCID_$trunkid', 'OUTFAIL_$trunkid', 'OUTKEEPCID_$trunkid',
+				'OUTMAXCHANS_$trunkid', 'OUTPREFIX_$trunkid', 'OUT_$trunkid',
+				'OUTDISABLE_$trunkid'
+			)
+		";
+		sql($sqlstr);
+	}
+	out(_("done"));
+} else {
+	out(_("not needed"));
+}
+
+$sql = "
+CREATE TABLE `trunks_dialpatterns` 
+( 
+	`trunkid` INTEGER,
+	`rule` VARCHAR( 255 ) NOT NULL, 
+	`seq` INTEGER,
+	PRIMARY KEY  (`trunkid`, `rule`, `seq`) 
+) 
+";
+outn(_("Checking if trunks_dialpatterns table exists.."));
+$check = $db->query($sql);
+if(DB::IsError($check) && $check->getCode() != DB_ERROR_ALREADY_EXISTS) {
+	die_freepbx("Can not create trunks_dialpatterns table");
+} else if(DB::IsError($check) && $check->getCode() == DB_ERROR_ALREADY_EXISTS) {
+	out(_("already exists"));
+} else {
+	out(_("created"));
+	outn(_("loading table from localprefixes.conf.."));
+	$localPrefixFile = $amp_conf['ASTETCDIR']."/localprefixes.conf";
+	$conf == array();
+	__parse_DialRulesFile($localPrefixFile, $conf, $section);
+
+	$rules_arr = array();
+	foreach ($conf as $tname => $rules) {
+		$tid = ltrim($tname,'trunk-');
+		ksort($rules); //make sure they are in order
+		$seq = 1;
+		foreach ($rules as $rule) {
+			$rules_arr[] = array($tid,$rule,$seq);
+			$seq++;
+		}
+	}
+	$compiled = $db->prepare("INSERT INTO `trunks_dialpatterns` (trunkid, rule, seq) VALUES (?,?,?)");
+	$result = $db->executeMultiple($compiled,$rules_arr);
+	if(DB::IsError($result)) {
+		die_freepbx($result->getDebugInfo().'error populating trunks_dialpatterns table');	
+	}
+	out(_("loaded"));
+}
+
+// END of trunks migration code
 
 ?>
