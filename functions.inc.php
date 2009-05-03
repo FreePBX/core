2c00d1906adfbcc5a6b852596eb0e683081f7b45
Micke Carlsson <mickecamino@gmail.com>
2009-05-03 08:37:52 +0000

    Closes #3492, add license text to carios files and directories in core

--- functions.inc.php
+++ functions.inc.php
@@ -1,5 +1,21 @@
 <?php
-
+//This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    Copyright (C) 2004 Coalescent Systems Inc. (info@coalescentsystems.ca)
+//
 class core_conf {
 	var $_sip_general    = array();
 	var $_iax_general    = array();
--- page.zapchandids.php
+++ page.zapchandids.php
@@ -1,16 +1,20 @@
 <?php 
-/** Zap Channel DIDs display for FreePBX 2.4
- * Copyright 2006 Philippe Lindheimer - Astrogen LLC
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+// Zap Channel DIDs display for FreePBX
+// This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2006 Philippe Lindheimer - Astrogen LLC
 
 $display = 'zapchandids';
 $type = isset($_REQUEST['type']) ? $_REQUEST['type'] :  'setup';
