a6e2d9a33166a21d292d50f3b6eacd1d7b7bffaf
Moshe Brevda <mbrevda@gmail.com>
2010-01-08 07:25:39 +0000

    re #4000; help text clarification. Thanks mickecarlsson - next time do it your self :p

--- page.did.php
+++ page.did.php
@@ -260,7 +260,7 @@ if (isset($inroutes)) {
 		<tr><td colspan="2"><h5><?php echo _("Privacy")?><hr></h5></td></tr>
 
 		<tr>
-			<td><a href="#" class="info"><?php echo _("Privacy Manager")?><span><?php echo _('If no Caller ID is sent, Privacy Manager will asks the caller to enter their digit phone number. If a user has Call Screening enabled, the incoming caller will be asked to enter their CallerId here if enabled, and then to say their name once determined that the called user requires it.')?></span></a>:</td>
+			<td><a href="#" class="info"><?php echo _("Privacy Manager")?><span><?php echo _('If no Caller ID has been recevied, Privacy Manager will asks the caller to enter their phone number. If an user/extension has Call Screening enabled, the incoming caller will be be promted to say their name when the call reaches the user/extension.')?></span></a>:</td>
 			<td>
 				<select name="privacyman" tabindex="<?php echo ++$tabindex;?>">
 					<option value="0" <?php  echo ($privacyman == '0' ? 'SELECTED' : '')?>><?php echo _("No")?>
@@ -269,7 +269,7 @@ if (isset($inroutes)) {
 			</td>
 		</tr>
 		<tr class="pm_opts" <?php echo $privacyman == '0' ? 'style="display:none"':''?>>
-			<td><a href="#" class="info"><?php echo _("Max attempts")?><span><?php echo _('This sets the amount of atempts the caller has to enter a valid callerID')?></span></a>:</td>
+			<td><a href="#" class="info"><?php echo _("Max attempts")?><span><?php echo _('Number of atempts the caller has to enter a valid callerID')?></span></a>:</td>
 			<td>
 				<select name="pmmaxretries" tabindex="<?php echo ++$tabindex;?>">
 					<?php
@@ -282,7 +282,7 @@ if (isset($inroutes)) {
 			</td>
 		</tr>	
 		<tr class="pm_opts" <?php echo $privacyman == '0' ? 'style="display:none"':''?>>
-			<td><a href="#" class="info"><?php echo _("Min Length")?><span><?php echo _('This sets the minimum amount of digits a callerID needs to contain in order to be valid')?></span></a>:</td>
+			<td><a href="#" class="info"><?php echo _("Min Length")?><span><?php echo _('Minimum amount of digits callerID needs to contain in order to be concidered valid')?></span></a>:</td>
 			<td>
 				<select name="pmminlength" tabindex="<?php echo ++$tabindex;?>">
 					<?php
