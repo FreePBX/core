467bd588b5b83b344d489378893a709b151a7728
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-01-17 03:35:28 +0000

    more changes to use class methods and other tweaks and notes re #4740

--- page.advancedsettings.php
+++ page.advancedsettings.php
@@ -10,33 +10,36 @@ foreach ($getvars as $v){
 
 if($var['action'] === 'setkey') {
 	//set value, then return a value
-	if (core_advancedsettings_set_keys($var['keyword'], $var['value'])) {
-		echo 'ok';
-	} else {
-		echo 'error';
-	}
+  if ($freepbx_conf->conf_setting_exists($var['keyword'])) {
+		$freepbx_conf->set_conf_values(array($keyword => $value),true);
+  }
 	exit;
 }
-	//save all values if amportal.conf isnt writeable
-  // TODO: why do we save them here? if we are suppose to save them, shouldn't it be somewhere more generic?
-  //
-	if (!$amp_conf['amportal_canwrite']) {
-		foreach($amp_conf as $keyword => $val) {
-			core_advancedsettings_set_keys($keyword, $val);
-		}
-	}
 	echo '<script type="text/javascript">';
 	echo 'can_write_amportalconf = "'.$amp_conf['amportal_canwrite'] .'"; ';
 	echo 'amportalconf_error ="' . _('You must run \'amportal restart\' from the cli before you can save setting here.') . '"';
 	echo '</script>';
 	
 	echo '<div id="main_page">';
-    echo "<h2>"._("FreePBX Advanced Settings")."</h2>";
-    echo "Use extreme caution! Changes here can render your system inoperable. You are urged to backup before making any changes.<br /><br />";
+  echo "<h2>"._("FreePBX Advanced Settings")."</h2>";
+  echo "Use extreme caution! Changes here can render your system inoperable. You are urged to backup before making any changes.<br /><br />";
 
+	$conf = $freepbx_conf->get_conf_settings();
 
-	//TODO: build table properly with all required tags
-	$conf = core_advancedsettings_get_keys($var['level']);
+  // TODO: Need to:
+  //       (1) filter out ones above the level specified
+  //       (2) create sections where the section headers are:
+  //           (a) category if not blank
+  //           (b) Module Name if not blank (where we look up the proper module name since rawname is provided)
+  //
+  //       We need to track which level they are showing. One way to do it is to simply make that anther setting
+  //       and have a section called 'Advanced Settings Config' where they can set that (and have is sort to the
+  //       top. We could have choices for:
+  //       - Level (0-10)
+  //       - Show Hidden
+  //       - Show Readonly
+  //
+
 	echo '<input type="image" src="images/spinner.gif" style="display:none">';
 	echo '<table id="set_table">';
 	foreach ($conf as $c){
@@ -71,7 +74,7 @@ if($var['action'] === 'setkey') {
 
 		}
 		echo '</td>';
-		if($c['readonly'] != 'true'){
+		if(!$c['readonly']){
 			echo '<td><input type="image" class="adv_set_default" src="images/default-option.png" data-key="'.$c['keyword'].'" data-default="'.$c['defaultval'].'" name="default" title="'._('Revert to Default').'"></td>';
 			echo '<td class="savetd"><input type="image" class="save" src="images/accept.png" name="save" data-key="'.$c['keyword'].'" title="'._('Save').'"></td>';
 			//echo '<td><input type="image" class="delete"  src="images/trash.png" name="delete" data-key="'.$c['keyword'].'" title="'._('Delete').'"></td>'; 
