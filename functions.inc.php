424b23032f246b64bfa705f1e5d48627ffbcbcdd
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2013-04-16 00:35:44 +0000

    have core set rtp.conf settings if sipsettings module is not there

--- functions.inc.php
+++ functions.inc.php
@@ -141,26 +141,47 @@ class core_conf {
 		}
 	}
 	
-    function addRtpAdditional($section,$entries) {
-      $this->_rtp_additional[$section][] = $entries;
-    }
+	// If sipsettings isn't there this will try to set the rtp.conf value
+	//
+	function setDefaultRtp() {
+		// if we have sipsettings then we don't need to do anything
+		// it will be done there
+		if (function_exists('sipsettings_hookGet_config')) {
+			return true;
+		}
 
-    function generate_rtp_additional($ast_version) {
-	    $output = '';
-	    if (!empty($this->_rtp_additional)) {
-	      foreach ($this->_rtp_additional as $section => $entries) {
-	        $output .= "[".$section."]\n";
-	        foreach ($entries as $key => $entry) {
-				foreach ($entry as $ekey => $value) {
-	          		$output .= "$ekey=$value\n";
+		$sql = "SELECT value FROM admin WHERE variable = 'RTPSTART'";
+		$rtpstart = sql($sql,'getOne');
+		$rtpstart = $rtpstart ? $rtpstart : '10000';
+
+		$sql = "SELECT value FROM admin WHERE variable = 'RTPEND'";
+		$rtpend = sql($sql,'getOne');
+		$rtpend = $rtpend ? $rtpend : '20000';
+
+		$this->addRtpAdditional('general', array('rtpstart' => $rtpstart));
+		$this->addRtpAdditional('general', array('rtpend' => $rtpend));
+		return true;
+	}
+
+	function addRtpAdditional($section,$entries) {
+		$this->_rtp_additional[$section][] = $entries;
+	}
+
+	function generate_rtp_additional($ast_version) {
+		$output = '';
+		if (!empty($this->_rtp_additional)) {
+			foreach ($this->_rtp_additional as $section => $entries) {
+				$output .= "[".$section."]\n";
+				foreach ($entries as $key => $entry) {
+					foreach ($entry as $ekey => $value) {
+						$output .= "$ekey=$value\n";
+					}
 				}
-	        }
-	        $output .= "\n";
-	      }
-	    }
-	    return $output;
-
-    }
+				$output .= "\n";
+			}
+		}
+		return $output;
+	}
 
   function addSipNotify($section,$entries) {
     $this->_sip_notify[] = array('section' => $section, 'entries' => $entries);
@@ -970,6 +991,8 @@ function core_get_config($engine) {
 			// Now add to sip_general_addtional.conf
 			//
 			if (isset($core_conf) && is_a($core_conf, "core_conf")) {
+				$core_conf->setDefaultRtp();
+
 				$core_conf->addSipGeneral('disallow','all');
 				$core_conf->addSipGeneral('allow','ulaw');
 				$core_conf->addSipGeneral('allow','alaw');
