44ba6460a417cbab9772a79a8d7795c4ae9b000e
Rob Thomas <xrobau@gmail.com>
2014-01-05 05:25:39 +1000

    Update Core and PJSip

--- Core.class.php
+++ Core.class.php
@@ -1,20 +1,17 @@
 <?php
 // vim: set ai ts=4 sw=4 ft=php:
 
-class Core implements BMO {
+class Core extends BMO {
+
 	public function __construct($freepbx = null) {
 		if ($freepbx == null)
 			throw new Exception("Not given a FreePBX Object");
 
-		$this->FreePBX = $freepbx;
-		$this->db = $freepbx->Database;
-		
 		//Hackery-Jackery for Core only really
 		if(!class_exists('PJSip') && file_exists(__DIR__.'/functions.inc/PJSip.class.php')) {
 			include(__DIR__.'/functions.inc/PJSip.class.php');
-			$this->FreePBX->PJSip = new PJSip($this->FreePBX);
+			FreePBX::create()->PJSip = new PJSip(FreePBX::create());
 		}
-		
 	}
 	
 	public function install() {
