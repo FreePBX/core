93f7450f85ac3df0d5a18360f592ba7d7484ee0e
Rob Thomas <xrobau@gmail.com>
2014-01-13 16:02:05 +1000

    Start work on astmodules

--- Core.class.php
+++ Core.class.php
@@ -1,20 +1,16 @@
 <?php
 // vim: set ai ts=4 sw=4 ft=php:
 
-class Core implements BMO {
+class Core extends FreePBX_Helpers implements BMO  {
+
 	public function __construct($freepbx = null) {
-		if ($freepbx == null)
-			throw new Exception("Not given a FreePBX Object");
 
-		$this->FreePBX = $freepbx;
-		$this->db = $freepbx->Database;
-
+		parent::__construct($freepbx);
 		//Hackery-Jackery for Core only really
 		if(!class_exists('PJSip') && file_exists(__DIR__.'/functions.inc/PJSip.class.php')) {
 			include(__DIR__.'/functions.inc/PJSip.class.php');
 			$this->FreePBX->PJSip = new PJSip($this->FreePBX);
 		}
-
 	}
 
 	public function install() {
@@ -28,4 +24,13 @@ class Core implements BMO {
 
 	public function restore($backup) {
 	}
+
+	public function doTests($db) {
+		return true;
+	}
+
+	public function doConfigPageInit($page) {
+		return true;
+	}
+
 }
---
+++ page.astmodules.php
@@ -0,0 +1,5 @@
+<?php
+
+$bmo = FreePBX::create()->Core;
+
+print "Select modules to do stuff with..<br />\n";
