91dd6a1dae5e261ebcb21b61e577a97e7f9a90df
Rob Thomas <xrobau@gmail.com>
2014-01-14 13:13:25 +1000

    Basic GUI Elements checkin.

--- page.astmodules.php
+++ page.astmodules.php
@@ -1,5 +1,26 @@
 <?php
 
-$bmo = FreePBX::create()->Core;
+global $currentcomponent;
 
-print "Select modules to do stuff with..<br />\n";
+$cc =& $currentcomponent;
+
+$mods = FreePBX::create()->ModulesConf();
+
+$cc->addguielem("_top", new gui_pageheading(null, _("Asterisk Modules")));
+$cc->addguielem("_top", new guitext(null, _("Note that this is for ASTERISK modules, not FreePBX Modules.")));
+
+$pc = $mods->ProcessedConfig['modules'];
+
+unset($pc['autoload']);
+
+foreach ($pc as $type => $filename) {
+	if (is_array($filename)) {
+		foreach($filename as $file) {
+			$cc->addguielem($type, new guitext(null, $file));
+		}
+	} else {
+		$cc->addguielem($type, new guitext(null, $filename));
+	}
+
+}
+
