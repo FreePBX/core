5d807859a6c4b3bd04b9b41f1760193aa0cc102e
Rob Thomas <xrobau@gmail.com>
2014-03-17 06:47:29 +1000

    Tidy up ancient checks in macro-user-callerid, add minor D&U fix

--- functions.inc.php
+++ functions.inc.php
@@ -2923,12 +2923,16 @@ function core_get_config($engine) {
 
 			$ext->add($context, $exten, '', new ext_set('TOUCH_MONITOR','${UNIQUEID}'));
 			// make sure AMPUSER is set if it doesn't get set below
-			$ext->add($context, $exten, '', new ext_set('AMPUSER', '${IF($["foo${AMPUSER}" = "foo"]?${CALLERID(number)}:${AMPUSER})}'));
+			$ext->add($context, $exten, '', new ext_set('AMPUSER', '${IF($["${AMPUSER}" = ""]?${CALLERID(number)}:${AMPUSER})}'));
 			$ext->add($context, $exten, '', new ext_gotoif('$["${CUT(CHANNEL,@,2):5:5}"="queue" | ${LEN(${AMPUSERCIDNAME})}]', 'report'));
 			$ext->add($context, $exten, '', new ext_execif('$["${REALCALLERIDNUM:1:2}" = ""]', 'Set', 'REALCALLERIDNUM=${CALLERID(number)}'));
 			$ext->add($context, $exten, '', new ext_set('AMPUSER', '${DB(DEVICE/${REALCALLERIDNUM}/user)}'));
+
+			// Device & User: If they're not signed in, then they can't do anything.
+			$ext->add($context, $exten, '', new ext_gotoif('$["${AMPUSER}" = "none"]', 'limit'));
+
 			$ext->add($context, $exten, '', new ext_set('AMPUSERCIDNAME', '${DB(AMPUSER/${AMPUSER}/cidname)}'));
-			$ext->add($context, $exten, '', new ext_gotoif('$["x${AMPUSERCIDNAME:1:2}" = "x"]', 'report'));
+			$ext->add($context, $exten, '', new ext_gotoif('$["${AMPUSERCIDNAME:1:2}" = ""]', 'report'));
 
 			// user may masquerade as a different user internally, so set the internal cid as indicated
 			// but keep the REALCALLERID which is used to determine their true identify and lookup info
