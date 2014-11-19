4ce1c7c84cae202ecf27e71b8f0f9bfc41b2fe69
Andrew Nagy <andrew.nagy@the159.com>
2014-11-18 23:21:17 -0500

    new page views and looks and feels

---
+++ assets/js/devices/devices.js
@@ -0,0 +1,46 @@
+var loc = window.location.hash.replace("#", "");
+if (loc !== "" && $("#" + loc + ".info-pane").length > 0) {
+	$(".info-pane").addClass("hidden");
+	$(".change-tab").removeClass("active");
+	$("#" + loc + ".info-pane").removeClass("hidden");
+	$(".change-tab[data-name='" + loc + "']").addClass("active");
+}
+$(".change-tab").click(function(event) {
+	var pos = document.body.scrollTop,
+			id = $(this).data("name");
+	if ($(this).hasClass("active")) {
+		event.stopPropagation();
+		event.preventDefault();
+		return true;
+	}
+	$(".info-pane").addClass("hidden");
+	$(".change-tab").removeClass("active");
+	$(this).addClass("active");
+	$("#" + id).removeClass("hidden");
+	location.hash = id;
+	document.body.scrollTop = document.documentElement.scrollTop = pos;
+	event.stopPropagation();
+	event.preventDefault();
+});
+$(".ext-container .fa.fa-question-circle").hover(function() {
+	var id = $(this).data("id");
+	$("#" + id + "-help").addClass("active");
+}, function() {
+	var id = $(this).data("id");
+	$("#" + id + "-help").removeClass("active");
+});
+$(".ext-container input, .ext-container select, .ext-container textarea").focus(function() {
+	var id = $(this).parents(".form-group").data("id");
+	$("#" + id + "-help").addClass("active");
+});
+$(".ext-container input, .ext-container select, .ext-container textarea").blur(function() {
+	var id = $(this).parents(".form-group").data("id");
+	$("#" + id + "-help").removeClass("active");
+});
+$(".types .fa.fa-question-circle").hover(function() {
+	var id = $(this).data("id");
+	$("#" + id + "-help").addClass("active");
+}, function() {
+	var id = $(this).data("id");
+	$("#" + id + "-help").removeClass("active");
+});
---
+++ views/users.php
@@ -0,0 +1,91 @@
+<div class="ext-container">
+  <form class="popover-form fpbx-submit" name="frm_users" action="<?php echo $action?>" method="post" data-fpbx-delete="config.php?display=users&amp;extdisplay=<?php echo $_REQUEST['extdisplay'] ?>&amp;action=del" role="form">
+    <?php foreach ( $html['top'] as $elem ) {
+      echo $elem['html'];
+    } ?>
+    <?php foreach ( $html['bottom'] as $elem ) {
+      echo $elem['html'];
+    } ?>
+    <ul class="nav nav-tabs" role="tablist">
+      <?php foreach(array_keys($html['middle']) as $category) { ?>
+        <li data-name="<?php echo strtolower($category)?>" class="change-tab <?php echo ($active == strtolower($category)) ? 'active' : ''?>"><a href="#<?php echo strtolower($category)?>"><?php echo ucfirst($category)?></a></li>
+      <?php $c++;} ?>
+    </ul>
+    <div class="display">
+      <?php foreach($html['middle'] as $category => $sections) { ?>
+        <div id="<?php echo strtolower($category)?>" class="info-pane <?php echo ($active == strtolower($category)) ? '' : 'hidden'?>">
+          <div class="container-fluid">
+            <?php foreach($sections as $section => $elements) { ?>
+              <div class="section-title" data-for="<?php echo strtolower($section)?>"><h3><i class="fa fa-minus"></i> <?php echo $section?></h3></div>
+              <div class="section" data-id="<?php echo strtolower($section)?>">
+                <?php foreach($elements as $elem) { ?>
+                  <?php if(!empty($elem['prompttext'])) {?>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="row">
+                          <div class="form-group" data-id="<?php echo $elem['name']?>">
+                            <div class="col-md-3">
+                              <label class="control-label" for="<?php echo $elem['name']?>"><?php echo $elem['prompttext']?></label>
+                              <?php if(!empty($elem['helptext'])) { ?>
+                                <i class="fa fa-question-circle" data-id="<?php echo $elem['name']?>"></i>
+                              <?php } ?>
+                            </div>
+                            <div class="col-md-9"><?php echo $elem['html']?></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <?php if(!empty($elem['helptext'])) { ?>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <span id="<?php echo $elem['name']?>-help" class="help-block"><?php echo $elem['helptext']?></span>
+                        </div>
+                      </div>
+                    <?php } ?>
+                  <?php } else { ?>
+                    <div class="row">
+                      <div class="col-md-12 element-container" data-id="<?php echo $elem['name']?>"><?php echo $elem['html']?></div>
+                    </div>
+                  <?php } ?>
+                <?php } ?>
+              </div>
+              <br/>
+            <?php } ?>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
+    <?php foreach($hiddens as $hidden) {?>
+      <?php echo $hidden['html'];?>
+    <?php } ?>
+  </form>
+</div>
+<script>
+  var theForm = document.frm_users;
+  <?php
+    $formname = "frm_users";
+    $htmlout = '';
+    foreach($jsfuncs as $function => $scripts) {
+      switch($function) {
+        case 'onsubmit()':
+          $htmlout .= "function ".$formname."_onsubmit(theForm) {\n";
+          foreach($scripts as $data) {
+            $htmlout .= $data;
+          }
+          $htmlout .= "\treturn true;}\n";
+        break;
+        default:
+          $htmlout .= "function ".$formname."_$function {\n";
+          foreach($scripts as $data) {
+            $htmlout .= $data;
+          }
+          $htmlout .= "}\n";
+        break;
+      }
+    }
+    echo $htmlout;
+  ?>
+  $("form").submit(function() {
+    return frm_users_onsubmit(this);
+  });
+</script>
