9b0924bb2310e8f21e72e1d97e8d39fe3c08773c
Andrew Nagy <andrew.nagy@the159.com>
2014-12-15 19:09:13 -0500

    better descriptions taken from 2.7

--- page.routing.php
+++ page.routing.php
@@ -535,15 +535,14 @@ if ($extdisplay) { // editing
     ?>
     <tr>
       <td colspan="2"><h5>
-        <a href=# class="info"><?php echo _("Dial Patterns that will use this Route")?><span>
-          <?php echo _("A Dial Pattern is a unique set of digits that will select this route and send the call to the designated trunks. If a dialed pattern matches this route, no subsequent routes will be tried. If Time Groups are enabled, subsequent routes will be checked for matches outside of the designated time(s).")?><br /><br /><b><?php echo _("Rules:")?></b><br />
-            <b>X</b>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 0-9")?><br />
-            <b>Z</b>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 1-9")?><br />
-            <b>N</b>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 2-9")?><br />
-            <b>[1237-9]</b>&nbsp;   <?php echo _("matches any digit in the brackets (example: 1,2,3,7,8,9)")?><br />
-            <b>.</b>&nbsp;&nbsp;&nbsp; <?php echo _("wildcard, matches one or more dialed digits")?> <br />
-            <br/><?php echo _("Enter one dial pattern per line")?>
-            </span></a>
+        <a href=# class="info"><?php echo _("Dial Patterns that will use this Route")?><span><?php echo _("A Dial Pattern is a unique set of digits that will select this trunk. Enter one dial pattern per line.")?><br><br><b><?php echo _("Rules:")?></b><br>
+          <strong>X</strong>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 0-9")?><br>
+          <strong>Z</strong>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 1-9")?><br>
+          <strong>N</strong>&nbsp;&nbsp;&nbsp; <?php echo _("matches any digit from 2-9")?><br>
+          <strong>[1237-9]</strong>&nbsp;   <?php echo _("matches any digit or letter in the brackets (in this example, 1,2,3,7,8,9)")?><br>
+          <strong>.</strong>&nbsp;&nbsp;&nbsp; <?php echo _("wildcard, matches one or more characters")?> <br>
+          <strong>|</strong>&nbsp;&nbsp;&nbsp; <?php echo _("separates a dialing prefix from the number (for example, 9|NXXXXXX would match when some dialed \"95551234\" but would only pass \"5551234\" to the trunks)")?><br />
+          <strong>/</strong>&nbsp;&nbsp;&nbsp; <?php echo _("appended to a dial pattern, matches a callerid or callerid pattern (for example, NXXXXXX/104 would match only if dialed by extension \"104\")")?></span></a>
           <hr></h5></td>
         </tr>
     <?php
