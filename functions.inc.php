7a75b60e49be9ab0082be9a9cf6a7f3b11fd05ce
Andrew Nagy <andrew.nagy@the159.com>
2015-02-02 17:51:50 -0500

    FREEPBX-8625 When saving device in adhoc mode, user gets logout automatically

--- functions.inc.php
+++ functions.inc.php
@@ -4544,7 +4544,7 @@ function core_devices_add($id,$tech,$dial,$devicetype,$user,$description,$emerge
 }
 
 function core_devices_del($account,$editmode=false){
-	return FreePBX::Core()->delDevice($account,$editmode=false);
+	return FreePBX::Core()->delDevice($account,$editmode);
 }
 
 function core_devices_get($account){
