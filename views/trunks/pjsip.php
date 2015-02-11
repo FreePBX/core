b30e8e2297306782745e447d348cb1770a7cd604
Andrew Nagy <andrew.nagy@the159.com>
2015-02-11 12:26:52 -0500

    gettext string conversion

--- views/trunks/pjsip.php
+++ views/trunks/pjsip.php
@@ -5,7 +5,7 @@
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Configuration Mode")?><span><?php echo _("")?></span></a>: 
+		<a href=# class="info"><?php echo _("Configuration Mode")?><span></span></a>:
 	</td><td>
 		<select id="configmode" name="configmode">
 			<option value="simple">Simple</option>
@@ -15,119 +15,119 @@
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Permanent Auth Rejection")?><span><?php echo _("Determines whether failed authentication challenges are treated as permanent failures.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Permanent Auth Rejection")?><span><?php echo _("Determines whether failed authentication challenges are treated as permanent failures.")?></span></a>:
 	</td><td>
 		<input type="checkbox" name="auth_rejection_permanent" <?php echo ($auth_rejection_permanent == 'on') ? 'checked' : ''?>/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Retry Interval")?><span><?php echo _("Maximum number of registration attempts.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Retry Interval")?><span><?php echo _("Maximum number of registration attempts.")?></span></a>:
 	</td><td>
 		<input type="text" size="8" name="retry_interval" value="<?php echo $retry_interval?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Expiration")?><span><?php echo _("Expiration time for registrations in seconds.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Expiration")?><span><?php echo _("Expiration time for registrations in seconds.")?></span></a>:
 	</td><td>
 		<input type="text" size="8" name="expiration" value="<?php echo $expiration?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Forbidden Retry Interval")?><span><?php echo _("Interval used when receiving a 403 Forbidden response.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Forbidden Retry Interval")?><span><?php echo _("Interval used when receiving a 403 Forbidden response.")?></span></a>:
 	</td><td>
 		<input type="text" size="8" name="forbidden_retry_interval" value="<?php echo $forbidden_retry_interval?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Max Retries")?><span><?php echo _("Maximum number of registration attempts.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Max Retries")?><span><?php echo _("Maximum number of registration attempts.")?></span></a>:
 	</td><td>
 		<input type="text" size="8" name="max_retries" value="<?php echo $max_retries?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Qualify Frequency")?><span><?php echo _("Interval at which to qualify.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Qualify Frequency")?><span><?php echo _("Interval at which to qualify.")?></span></a>:
 	</td><td>
 		<input type="text" size="8" name="qualify_frequency" value="<?php echo $qualify_frequency?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Username")?><span><?php echo _("")?></span></a>: 
+		<a href=# class="info"><?php echo _("Username")?><span></span></a>:
 	</td><td>
 		<input type="text" name="username" value="<?php echo $username?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Secret")?><span><?php echo _("")?></span></a>: 
+		<a href=# class="info"><?php echo _("Secret")?><span></span></a>: 
 	</td><td>
 		<input type="text" name="secret" value="<?php echo $secret?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("SIP Server")?><span><?php echo _("SIP Server Address.")?></span></a>: 
+		<a href=# class="info"><?php echo _("SIP Server")?><span><?php echo _("SIP Server Address.")?></span></a>:
 	</td><td>
 	<input type="text" name="sip_server" value="<?php echo $sip_server?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("SIP Server Port")?><span><?php echo _("SIP Server Port.")?></span></a>: 
+		<a href=# class="info"><?php echo _("SIP Server Port")?><span><?php echo _("SIP Server Port.")?></span></a>:
 	</td><td>
 		<input type="text" name="sip_server_port" value="<?php echo !empty($sip_server_port) ? $sip_server_port : '5060'?>"/>
 	</td>
 </tr>
 <tr class="advancedpjsip" style="display:none;">
 	<td>
-		<a href=# class="info"><?php echo _("Client URI")?><span><?php echo _("Client SIP URI used when attemping outbound registration")?></span></a>: 
+		<a href=# class="info"><?php echo _("Client URI")?><span><?php echo _("Client SIP URI used when attemping outbound registration")?></span></a>:
 	</td><td>
 	<input type="text" name="client_uri" value="<?php echo $client_uri?>"/>
 	</td>
 </tr>
 <tr class="advancedpjsip" style="display:none;">
 	<td>
-		<a href=# class="info"><?php echo _("Server URI")?><span><?php echo _("SIP URI of the server to register against")?></span></a>: 
+		<a href=# class="info"><?php echo _("Server URI")?><span><?php echo _("SIP URI of the server to register against")?></span></a>:
 	</td><td>
 		<input type="text" name="server_uri" value="<?php echo $server_uri?>"/>
 	</td>
 </tr>
 <tr class="advancedpjsip" style="display:none;">
 	<td>
-		<a href=# class="info"><?php echo _("AOR Contact")?><span><?php echo _("Permanent contacts assigned to AoR")?></span></a>: 
+		<a href=# class="info"><?php echo _("AOR Contact")?><span><?php echo _("Permanent contacts assigned to AoR")?></span></a>:
 	</td><td>
 		<input type="text" name="aor_contact" value="<?php echo $aor_contact?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Outbound Proxy")?><span><?php echo _("Outbound Proxy.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Outbound Proxy")?><span><?php echo _("Outbound Proxy.")?></span></a>:
 	</td><td>
 		<input type="text" name="outbound_proxy" value="<?php echo $outbound_proxy?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Contact User")?><span><?php echo _("Contact User to use in request.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Contact User")?><span><?php echo _("Contact User to use in request.")?></span></a>:
 	</td><td>
 		<input type="text" name="contact_user" value="<?php echo $contact_user?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Context")?><span><?php echo _("Context to send the Inbound Call to.")?></span></a>: 
+		<a href=# class="info"><?php echo _("Context")?><span><?php echo _("Context to send the Inbound Call to.")?></span></a>:
 	</td><td>
 		<input type="text" name="context" value="<?php echo !empty($context) ? $context : 'from-pstn'?>"/>
 	</td>
 </tr>
 <tr>
 	<td>
-		<a href=# class="info"><?php echo _("Transport")?><span><?php echo _("The Transport to use for connection")?></span></a>: 
+		<a href=# class="info"><?php echo _("Transport")?><span><?php echo _("The Transport to use for connection")?></span></a>:
 	</td>
 	<td>
 		<select name="transport">
