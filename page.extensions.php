6b398f59675ae508d97a452b64da418a23d6dd8b
Andrew Nagy <andrew.nagy@the159.com>
2015-04-21 20:57:20 -0400

    FREEPBX-8993 popovers in 13

--- page.extensions.php
+++ page.extensions.php
@@ -4,7 +4,7 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 ?>
 <div class="fpbx-container container-fluid">
 	<div class="row">
-		<div class="col-sm-9">
+		<div class="col-sm-9 <?php echo (isset($_REQUEST['fw_popover']) && empty($_REQUEST['tech_hardware']) && empty($_REQUEST['extdisplay'])) ? "hidden":""?>">
 			<?php
 			// If this is a popOver, we need to set it so the selection of device type does not result
 			// in the popover closing because config.php thinks it was the process function. Maybe
@@ -108,9 +108,11 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 				echo $currentcomponent->generateconfigpage(__DIR__."/views/extensions.php");
 			} ?>
 		</div>
-		<div class="col-sm-3 hidden-xs bootnav">
+		<div class="col-sm-3 hidden-xs bootnav <?php echo (isset($_REQUEST['fw_popover']) && (!empty($_REQUEST['tech_hardware']) || !empty($_REQUEST['extdisplay']))) ? "hidden":""?>">
 			<div class="list-group">
-				<a href="?display=extensions<?php echo $popover?>" class="list-group-item"><i class="fa fa-list"></i> <?php echo _('List Extensions')?></a>
+				<?php if (!isset($_REQUEST['fw_popover'])) { ?>
+					<a href="?display=extensions<?php echo $popover?>" class="list-group-item"><i class="fa fa-list"></i> <?php echo _('List Extensions')?></a>
+				<?php } ?>
 				<?php
 					foreach(FreePBX::Core()->getAllDriversInfo() as $driver) {
 						?><a href="?display=extensions&amp;tech_hardware=<?php echo $driver['hardware']?><?php echo $popover?>" class="list-group-item"><?php echo sprintf(_("Add New %s Extension"), $driver['shortName'])?></a><?php
