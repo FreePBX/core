f7d12cc05d19afa65bf3be285e2e6c04dbb3ae0f
Rob Thomas <xrobau@gmail.com>
2015-05-18 10:18:50 +1000

    FREEPBX-7280 - Convert macro-dial
    diff /tmp/macro-dial /tmp/macro-dial-new
    1a2
    > include => macro-dial-custom
    5,6c6,7
    < exten => s,n,NoOp(Returned from dialparties with no extensions to call and DIALSTATUS: ${DIALSTATUS})
    < exten => s,n+2(normdial),Dial(${ds})
    ---
    > exten => s,n,Noop(Returned from dialparties with no extensions to call > and DIALSTATUS: ${DIALSTATUS})
    > exten => s,n+2(normdial),Dial(${ds},)
    8,9c9,10
    < exten => s,n,GosubIf($[("${SCREEN}" != "" & ("${DIALSTATUS}" = "TORTURE" | "${DIALSTATUS}" = "DONTCALL"))  | "${DIALSTATUS}" = "ANSWER"]?${DIALSTATUS},1)
    < exten => s,20(huntdial),NoOp(Returned from dialparties with hunt groups to dial )
    ---
    > exten => s,n,GosubIf($[("${SCREEN}" != "" & ("${DIALSTATUS}" = > "TORTURE" | "${DIALSTATUS}" = "DONTCALL"))  | "${DIALSTATUS}" = > "ANSWER"]?${DIALSTATUS},1())
    > exten => s,20(huntdial),Noop(Returned from dialparties with hunt > groups to dial )
    12c13
    < exten => s,n,NoOp(Returning there are no members left in the hunt group to ring)
    ---
    > exten => s,n,Noop(Returning there are no members left in the hunt > group to ring)
    25c26
    < exten => s,n(a42),Dial(${${HuntMember}}${ds})
    ---
    > exten => s,n(a42),Dial(${${HuntMember}}${ds},)
    36,37c37,38
    < exten => NOANSWER,n,Return
    < exten => NOANSWER,n(bye),Macro(hangupcall)
    ---
    > exten => NOANSWER,n,Return()
    > exten => NOANSWER,n(bye),Macro(hangupcall,)
    39c40
    < exten => TORTURE,n,Macro(hangupcall)
    ---
    > exten => TORTURE,n,Macro(hangupcall,)
    44c45
    < exten => DONTCALL,n,Macro(hangupcall)
    ---
    > exten => DONTCALL,n,Macro(hangupcall,)
    47c48
    < exten => h,1,Macro(hangupcall)
    ---
    > exten => h,1,Macro(hangupcall,)

---
+++ functions.inc/macro-dial.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * macro-dial
+ *
+ * Originally a hand-written macro in extensions.conf, it has now been ported
+ * to generated dialplan, so it can be altered programatically.
+ *
+ * Note: This is EXACTLY THE SAME as the hand written code. If there is anything
+ * here that doesn't seem correct, that's the way it's always been, so look harder
+ * before you change anything.
+ */
+
+$c = "macro-dial-xxx";
+$s = "s";
+
+$ext->add($c,$s,'', new ext_gotoif('$["${MOHCLASS}" = ""]', 'dial'));
+$ext->add($c,$s,'', new ext_set('CHANNEL(musicclass)', '${MOHCLASS}'));
+$ext->add($c,$s,'dial', new ext_agi('dialparties.agi'));
+$ext->add($c,$s,'', new ext_noop('Returned from dialparties with no extensions to call and DIALSTATUS: ${DIALSTATUS}'));
+$ext->add($c,$s,'normdial', new ext_dial('${ds}', ''), 'n', 2); // dialparties will set the priority to 10 if $ds is not null
+$ext->add($c,$s,'', new ext_set('DIALSTATUS', '${IF($["${DIALSTATUS_CW}"!="" ]?${DIALSTATUS_CW}:${DIALSTATUS})}'));
+$ext->add($c,$s,'', new ext_gosubif('$[("${SCREEN}" != "" & ("${DIALSTATUS}" = "TORTURE" | "${DIALSTATUS}" = "DONTCALL"))  | "${DIALSTATUS}" = "ANSWER"]', '${DIALSTATUS},1'));
+
+// This is bad and terrible, and don't do this. But we can't, currently,
+// explicitly set a priority.
+$ext->_exts[$c][" s "][] = array('basetag' => 20, 'tag' => 'huntdial', 'addpri' => false, 'cmd' => new ext_noop('Returned from dialparties with hunt groups to dial '));
+
+$ext->add($c,$s,'', new ext_set('HuntLoop', 0));
+$ext->add($c,$s,'a22', new ext_gotoif('$[${HuntMembers} >= 1]', 'a30')); // if this is from rg-group, don't strip prefix
+$ext->add($c,$s,'', new ext_noop('Returning there are no members left in the hunt group to ring'));
+$ext->add($c,$s,'a30', new ext_set('HuntMember', 'HuntMember${HuntLoop}'), 'n', 2);
+$ext->add($c,$s,'', new ext_gotoif('$[$["${CALLTRACE_HUNT}" != "" ] & $[$["${RingGroupMethod}" = "hunt" ] | $["${RingGroupMethod}" = "firstavailable"] | $["${RingGroupMethod}" = "firstnotonphone"]]]', 'a32', 'a35'));
+$ext->add($c,$s,'a32', new ext_set('CT_EXTEN', '${CUT(FILTERED_DIAL,,$[${HuntLoop} + 1])}'));
+$ext->add($c,$s,'', new ext_set('DB(CALLTRACE/${CT_EXTEN})', '${CALLTRACE_HUNT}'));
+$ext->add($c,$s,'', new ext_goto('a42', 's'));
+$ext->add($c,$s,'a35', new ext_gotoif('$[$["${CALLTRACE_HUNT}" != "" ] & $["${RingGroupMethod}" = "memoryhunt" ]]', 'a36', 'a50'));
+$ext->add($c,$s,'a36', new ext_set('CTLoop', 0));
+$ext->add($c,$s,'a37', new ext_gotoif('$[${CTLoop} > ${HuntLoop}]', 'a42')); // if this is from rg-group, don't strip prefix
+$ext->add($c,$s,'', new ext_set('CT_EXTEN', '${CUT(FILTERED_DIAL,,$[${CTLoop} + 1])}'));
+$ext->add($c,$s,'', new ext_set('DB(CALLTRACE/${CT_EXTEN})', '${CALLTRACE_HUNT}'));
+$ext->add($c,$s,'', new ext_set('CTLoop', '$[1 + ${CTLoop}]'));
+$ext->add($c,$s,'', new ext_goto('a37', 's'));
+$ext->add($c,$s,'a42', new ext_dial('${${HuntMember}}${ds}', ''));
+$ext->add($c,$s,'', new ext_gotoif('$["${DIALSTATUS}" = "ANSWER"]', 'ANSWER,1'));
+$ext->add($c,$s,'', new ext_set('HuntLoop', '$[1 + ${HuntLoop}]'));
+$ext->add($c,$s,'', new ext_gotoif('$[$[$["foo${RingGroupMethod}" != "foofirstavailable"] & $["foo${RingGroupMethod}" != "foofirstnotonphone"]] | $["foo${DialStatus}" = "fooBUSY"]]', 'a46'));
+$ext->add($c,$s,'', new ext_set('HuntMembers', (string) '0')); // PHP-ism. String a zero, just because.
+$ext->add($c,$s,'a46', new ext_set('HuntMembers', '$[${HuntMembers} - 1]'));
+$ext->add($c,$s,'', new ext_goto('a22', 's'));
+$ext->add($c,$s,'a50', new ext_noop('Deleting: CALLTRACE/${CT_EXTEN} ${DB_DELETE(CALLTRACE/${CT_EXTEN})}'));
+$ext->add($c,$s,'', new ext_goto('a42', 's'));
+
+$s = 'NOANSWER';
+$ext->add($c,$s,'', new ext_macro('vm', '${SCREEN_EXTEN},BUSY,${IVR_RETVM}'));
+$ext->add($c,$s,'', new ext_gotoif('$["${IVR_RETVM}" != "RETURN" | "${IVR_CONTEXT}" = ""]', 'bye'));
+$ext->add($c,$s,'', new ext_return());
+$ext->add($c,$s,'bye', new ext_macro('hangupcall'));
+
+$s = 'TORTURE';
+$ext->add($c,$s,'', new ext_goto('app-blackhole,musiconhold,1'));
+$ext->add($c,$s,'', new ext_macro('hangupcall'));
+
+$s = 'DONTCALL';
+$ext->add($c,$s,'', new ext_answer());
+$ext->add($c,$s,'', new ext_wait(1));
+$ext->add($c,$s,'', new ext_zapateller());
+$ext->add($c,$s,'', new ext_playback('ss-noservice'));
+$ext->add($c,$s,'', new ext_macro('hangupcall'));
+
+$s = 'ANSWER';
+$ext->add($c,$s,'', new ext_noop('Call successfully answered - Hanging up now'));
+$ext->add($c,$s,'', new ext_macro('hangupcall'));
+
+$ext->add($c,'h','', new ext_macro('hangupcall'));
