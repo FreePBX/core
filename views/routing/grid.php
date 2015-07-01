6b47e0adb55aad489172f28fba85614d81420882
James Finstrom <jfinstrom@sangoma.com>
2015-07-01 16:03:53 -0400

    FREEPBX-9607 Add lock icon to CID item in grid of extension override is set. Also added tool tips to all icons in grid

--- views/routing/grid.php
+++ views/routing/grid.php
@@ -12,31 +12,43 @@ foreach($routelist as $route){
 	$time_group_id = $route['time_group_id'];
 	$seq = $route['seq'];
 	$attributes = '';
+	$attributes .= '<a href="#" data-toggle="tooltip" data-placement="right" title="'._("Emergency Route").'">';
 	if($emergency_route != ''){
 		$attributes .= '<i class="fa fa-lg fa-ambulance fa-border text-success" rel="Emergency Route"></i>&nbsp;';
 	}else{
 		$attributes .= '<i class="fa fa-lg fa-ambulance fa-border text-muted" rel="Emergency Route"></i>&nbsp;';
 	}
+	$attributes .= '</a>';
+	$attributes .= '<a href="#" data-toggle="tooltip" data-placement="right" title="'._("Intra-Company Route").'">';
 	if($intracompany_route != ''){
 		$attributes .= '<i class="fa fa-lg  fa-building fa-border text-success" rel="Intra Company Route"></i>&nbsp;';
 	}else{
 		$attributes .= '<i class="fa fa-lg  fa-building fa-border text-muted" rel="Intra Company Route"></i>&nbsp;';
 	}
+	$attributes .= '</a>';
+	$attributes .= '<a href="#" data-toggle="tooltip" data-placement="right" title="'._("Password Protected").'">';
 	if($password != ''){
 		$attributes .= '<i class="fa fa-lg fa-key fa-border text-success" rel="Password Set"></i>&nbsp;';
 	}else{
 		$attributes .= '<i class="fa fa-lg fa-key fa-border text-muted" rel="Password Set"></i>&nbsp;';
 	}
+	$attributes .= '</a>';
+	$attributes .= '<a href="#" data-toggle="tooltip" data-placement="right" title="'._("Time Group Assigned").'">';
 	if($time_group_id != ''){
 		$attributes .= '<i class="fa fa-lg fa-clock-o fa-border text-success" rel="Time Group Assigned"></i>&nbsp;';
 	}else{
 		$attributes .= '<i class="fa fa-lg fa-clock-o fa-border text-muted" rel="Time Group Assigned"></i>&nbsp;';
 	}
+	$attributes .= '</a>';
+	$cidicon = '';
+	if($route['outcid_mode'] == "override_extension"){
+		$cidicon = '<a href="#" data-toggle="tooltip" data-placement="right" title="'._("Overrides Extension CID").'"><i class = "fa fa-lock"></i></a>';
+	}
 
 $routerows .= <<<HERE
 <tr id = "row$id"  data-id="$id" data-seq="$seq">
 <td><i class="fa fa-arrows"></i>&nbsp $name</td>
-<td>$outcid</td>
+<td>$outcid $cidicon</td>
 <td>$attributes</td>
 <td><a href="config.php?display=routing&view=form&id=$id&extdisplay=$id">
 	<i class="fa fa-edit"></i></a>&nbsp;&nbsp;
