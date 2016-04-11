daad0852846394009564436fefd23c20df99c720
Andrew Nagy <andrew.nagy@the159.com>
2016-04-11 10:42:06 -0700

    FREEPBX-12019 run trim against any timezone setting

--- install.php
+++ install.php
@@ -1614,19 +1614,19 @@ if (is_link('/etc/localtime')) {
 	// timezone in /usr/share/zoneinfo.
 	$filename = readlink('/etc/localtime');
 	if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
-		$timezone = substr($filename, 20);
+		$timezone = trim(substr($filename, 20));
 	}
 } elseif (file_exists('/etc/timezone')) {
 	// Ubuntu / Debian.
 	$data = file_get_contents('/etc/timezone');
 	if (!empty($data)) {
-		$timezone = $data;
+		$timezone = trim($data);
 	}
 } elseif (file_exists('/etc/sysconfig/clock')) {
 	// RHEL / CentOS
 	$data = @parse_ini_file('/etc/sysconfig/clock');
 	if (!empty($data['ZONE'])) {
-		$timezone = $data['ZONE'];
+		$timezone = trim($data['ZONE']);
 	}
 }
 
