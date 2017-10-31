66829cc017342abe00ec9643a440d5e3baae7bfa
Andrew Nagy <andrew.nagy@the159.com>
2017-10-31 10:05:42 -0700

    Restrict modules in attendedtransfer-rec

--- agi-bin/attendedtransfer-rec-restart.php
+++ agi-bin/attendedtransfer-rec-restart.php
@@ -1,12 +1,7 @@
 #!/usr/bin/env php
 <?php
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
-//	Copyright (C) 2012 HEHE Enterprises, LLC d.b.a. i9 Technologies
-//	Copyright 2013,2014 Schmooze Com Inc.
-
-//Bootstrap FreePBX
-$bootstrap_settings['freepbx_auth'] = false;
-include '/etc/freepbx.conf';
+//	Copyright 2017 Sangoma Technologies Inc
 
 $channel = $argv[1];
 $filename = $argv[2];
@@ -21,6 +16,14 @@ if($filename == NULL){
 	exit;
 }
 
+//Bootstrap FreePBX
+$bootstrap_settings['freepbx_auth'] = false;
+$restrict_mods = true;
+include '/etc/freepbx.conf';
+
+//jic these got overwritten inside of freepbx
+$channel = $argv[1];
+$filename = $argv[2];
+
 // start recording now
 $astman->mixmonitor($channel, "$filename", "ai(LOCAL_MIXMON_ID)");
-
