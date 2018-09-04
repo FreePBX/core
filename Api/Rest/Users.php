0ad6f860dfe3e7d2b1b7f92aa026d4ee079e675d
Andrew Nagy <andrew.nagy@the159.com>
2018-09-04 18:35:10 -0400

    add rest

---
+++ Api/Rest/Users.php
@@ -0,0 +1,43 @@
+<?php
+namespace FreePBX\modules\Core\Api\Rest;
+use FreePBX\modules\Api\Rest\Base;
+class Users extends Base {
+	protected $module = 'core';
+	public function setupRoutes($app) {
+
+		/**
+		 * @verb GET
+		 * @return - a list of users
+		 * @uri /core/users
+		 */
+		$app->get('/users', function ($request, $response, $args) {
+			$users = $this->freepbx->Core->getAllUsers();
+			return $response->withJson($users);
+		})->add($this->checkAllReadScopeMiddleware());
+
+		/**
+		 * @verb GET
+		 * @returns - a user resource
+		 * @uri /core/users/:id
+		 */
+		$app->get('/users/{id}', function ($request, $response, $args) {
+			$base = $this->freepbx->Core->getUser($args['id']);
+
+			// Now, find their voicemail information.
+			$z = file("/etc/asterisk/voicemail.conf");
+			foreach ($z as $line) {
+				$res = explode("=>", $line);
+				if (!isset($res[1]))
+				continue;
+
+				if (trim($res[0]) == trim($args['id'])) {
+					$base['vm'] = trim($res[1]);
+					return $response->withJson($base);
+				}
+			}
+
+			// No voicemail found.
+			return $response->withJson($base);
+		})->add($this->checkAllReadScopeMiddleware());
+	}
+}
