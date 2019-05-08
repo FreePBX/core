fa78da7cd60da42e673b6258178bffa6a5b12922
Philip Joseph <jphilip@sangoma.com>
2019-05-08 04:20:36 +0000

    FREEPBX-19451
    	merge conflict

--- Core.class.php
+++ Core.class.php
@@ -1402,6 +1402,7 @@ class Core extends FreePBX_Helpers implements BMO  {
 					$astman->database_put("DEVICE",$id."/user",$settings['user']['value']);
 				}
 			}
+			$astman->database_put("DEVICE",$id."/tech",$tech);
 			$astman->database_put("DEVICE",$id."/dial",$settings['dial']['value']);
 			$astman->database_put("DEVICE",$id."/type",$settings['devicetype']['value']);
 			$astman->database_put("DEVICE",$id."/default_user",$settings['user']['value']);
