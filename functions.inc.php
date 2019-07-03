b0992feac1defc439c934a7195e0c4050bac699e
Philip Joseph <jphilip@sangoma.com>
2019-07-03 12:10:13 +0530

    FREEPBX-20190 Issue with Queue Callback Call Confimation.
    	fixed

--- functions.inc.php
+++ functions.inc.php
@@ -2327,7 +2327,7 @@ function core_do_get_config($engine) {
 		$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}"="0"  & "${SHARED(ANSWER_STATUS,${FORCE_CONFIRM})}"=""]', 'toolate,1'));
 		$ext->add($context, $exten, '', new ext_gotoif('$["${BLKVM_CHANNEL}" !="" & "${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}"="0" & "${SHARED(ANSWER_STATUS,${BLKVM_CHANNEL})}"=""]', 'toolate,1'));
 		$ext->add($context, $exten, '', new ext_setvar("cfchannel", '${IF($[${REGEX("/from-queue/" ${UNIQCHAN})}]?${UNIQCHAN}:${BLKVM_CHANNEL})}'));
-		$ext->add($context, $exten, '', new ext_gotoif('$["${SHARED(BLKVM,${cfchannel})}"=""]', 'toolate,1'));
+		$ext->add($context, $exten, '', new ext_gotoif('$["${SHARED(BLKVM,${cfchannel})}"="" & ${QCALLBACK}=""]', 'toolate,1'));
 	} else {
 		$ext->add($context, $exten, '', new ext_gotoif('$["${FORCE_CONFIRM}" != ""]', 'skip'));
 		$ext->add($context, $exten, '', new ext_gotoif('$["${DB_EXISTS(RG/${ARG3}/${UNIQCHAN})}"="0"]', 'toolate,1'));
