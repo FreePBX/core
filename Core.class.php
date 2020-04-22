e1e74712c87ae5afae4f26e98baa7aa165097956
Vivek Mishra <vmishra@sangoma.com>
2020-04-22 21:38:35 +0000

    FREEI-1468: to update default_user value from extension advance setting form

--- Core.class.php
+++ Core.class.php
@@ -1588,7 +1588,7 @@ class Core extends \FreePBX_Helpers implements \BMO  {
 			$astman->database_put("DEVICE",$id."/tech",$tech);
 			$astman->database_put("DEVICE",$id."/dial",$settings['dial']['value']);
 			$astman->database_put("DEVICE",$id."/type",$settings['devicetype']['value']);
-			$astman->database_put("DEVICE",$id."/default_user",$settings['user']['value']);
+			$astman->database_put("DEVICE",$id."/default_user",$settings['defaultuser']['value']);
 			if($settings['emergency_cid']['value'] != '') {
 				$astman->database_put("DEVICE",$id."/emergency_cid",$settings['emergency_cid']['value']);
 			} else {
