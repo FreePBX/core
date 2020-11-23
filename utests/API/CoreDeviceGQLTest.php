b1327a11506a5b42a0abb483a075b64130519e15
Matt Brooks <mbrooks@sangoma.com>
2020-11-23 16:24:49 +0000

    Created unit test for coreDeviceQuery
    This PR creates a unit test for coreDeviceQuery using the
    ApiBaseTestCase class.

---
+++ utests/API/CoreDeviceGQLTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace FreepPBX\Core\utests;
+
+require_once('../api/utests/ApiBaseTestCase.php');
+
+use FreePBX\modules\Core;
+use Exception;
+use FreePBX\modules\Api\utests\ApiBaseTestCase;
+
+class CoreDeviceGQLTest extends ApiBaseTestCase {
+	protected static $core;
+	protected static $minTestExtension = 979000;
+	protected static $maxTestExtension = 979999;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		self::$core = self::$freepbx->Core;
+	}
+
+	public static function tearDownAfterClass() {
+		parent::tearDownAfterClass();
+		foreach(self::$core->getAllDevicesByType() as $device) {
+			if ($device['id'] >= self::$minTestExtension && $device['id'] <= self::$maxTestExtension) {
+				self::$core->delDevice($device['id']);
+			}
+		}
+	}
+
+	public function test_coreDeviceQuery_whenAllIsWell_shouldReturnDevice() {
+		$testExtension = 979898;
+
+		// clean up previous test
+		self::$core->delDevice($testExtension);
+
+		// create test device
+		$settings = self::$core->generateDefaultDeviceSettings(
+			'pjsip', 
+			$testExtension,
+			'pjsip test'
+		);
+		self::$core->addDevice($testExtension, 'pjsip', $settings);
+
+		$response = $this->request("query { 
+			coreDevice(id: \"{$testExtension}\") { 
+				id,
+				device_id,
+				description,
+				devicetype,
+				dial,
+				emergency_cid
+			}
+		}");
+
+		$json = json_decode((string)$response->getBody(), true);
+		$mutationId = base64_encode("coredevice:{$testExtension}");
+		$this->assertEquals($json, array(
+			'data' => array(
+				'coreDevice' => array(
+					'id' => $mutationId,
+					'device_id' => $testExtension,
+					'description' => 'pjsip test',
+					'devicetype' => 'fixed',
+					'dial' => "PJSIP/{$testExtension}",
+					'emergency_cid' => ''
+				)
+			)
+		));
+	}
+}
