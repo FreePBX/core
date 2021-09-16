7339b0641304403c7255238e436af9ab21fe2f12
S Tejaswini <stejaswini@sangoma.com>
2021-09-16 18:03:39 +0530

    Fixed the bulkhandler issue in Core Module

--- Core.class.php
+++ Core.class.php
@@ -3265,6 +3265,7 @@ class Core extends FreePBX_Helpers implements BMO  {
 	 */
 	public function bulkhandlerValidate($type, $rawData) {
 		switch ($type) {
+		$techType = array('pjsip', 'sip', 'virtual', 'iax2', 'dahdi', 'custom');
 		case 'extensions':
 			foreach ($rawData as $data) {
 
@@ -3276,6 +3277,9 @@ class Core extends FreePBX_Helpers implements BMO  {
 				}
 				if(empty($data['name'])){
 					return array("status" => false, "message" => _("Extension name is blank."));
+				}
+				if(!empty($data['tech']) && !in_array($data['tech'], $techType)) {
+					return array("status" => false, "message" => _("Please provide valid Device Technology"));
 				}
 			}
 			return array("status" => true);
