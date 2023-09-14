b51dc5ed0d4de2daabfe0c08dfa451dc7849d4e9
Akarsh MR <akarshmr@sangoma.com>
2023-09-14 15:17:54 +0530

    core module restore issue fix php 8.2

--- Restore/Corebase.php
+++ Restore/Corebase.php
@@ -1,6 +1,7 @@
 <?php
 namespace FreePBX\modules\Core\Restore;
 
+#[\AllowDynamicProperties]
 class Corebase {
 	public function __construct($freepbx,$transaction){
 		$this->className = substr(strrchr(get_class($this), '\\'), 1);
