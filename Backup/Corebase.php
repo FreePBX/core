7de45a1f9b9b9228d420e315a5e119f00198241a
Akarsh MR <akarshmr@sangoma.com>
2023-09-14 14:52:35 +0530

    core module backup issue fix php 8.2

--- Backup/Corebase.php
+++ Backup/Corebase.php
@@ -1,6 +1,7 @@
 <?php
 namespace FreePBX\modules\Core\Backup;
 
+#[\AllowDynamicProperties]
 class Corebase {
     public function __construct($freepbx, $id, $transaction){
         $this->className = substr(strrchr(get_class($this), '\\'), 1);
