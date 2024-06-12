ea71e1b2d4c84d0cd1906d13818691dcf24452d3
Philip Joseph <jphilip@sangoma.com>
2024-06-12 14:12:26 +0530

    issue-221 astdb values are added into backup and restore

--- Backup/Users.php
+++ Backup/Users.php
@@ -4,8 +4,14 @@ namespace FreePBX\modules\Core\Backup;
 
 class Users extends Corebase{
 	public function getConfigs(){
+		$users = $this->FreePBX->Database->query("SELECT * FROM users")->fetchAll(\PDO::FETCH_ASSOC);
+		$astdbConfigs = [];
+		foreach ($users as $user) {
+			$astdbConfigs[$user['extension']] = $this->FreePBX->Core->getAstdbConfigs($user['extension']);
+		}
 		return [
-			"users" => $this->FreePBX->Database->query("SELECT * FROM users")->fetchAll(\PDO::FETCH_ASSOC)
+			"users" => $users,
+			"astdbConfigs" => $astdbConfigs
 		];
 	}
 	public function getFiles(){
