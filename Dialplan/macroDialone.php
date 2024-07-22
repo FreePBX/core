b028eaab43443c4aac5177176fe31213d61e4ada
Prasanth Mohan <prasanthm@sangoma.com>
2024-07-22 01:45:11 -0500

    typo

--- Dialplan/macroDialone.php
+++ Dialplan/macroDialone.php
@@ -109,8 +109,8 @@ class macroDialone{
 
 		$ext->add('dialOne-with-exten', '_[+-X].', '', new \ext_dial('${DSTRING}', '${ARG1},${D_OPTIONS}${CWRING}b(func-apply-sipheaders^s^1)'));
 		$ext->add('dialOne-with-exten', '_[+-X].','', new \ext_return());
-		$ext->add('dialOne-with-exten', 'X', '', new \ext_dial('${DSTRING}', '${ARG1},${D_OPTIONS}${CWRING}b(func-apply-sipheaders^s^1)'));
-		$ext->add('dialOne-with-exten', 'X','', new \ext_return());
+		$ext->add('dialOne-with-exten', '_X', '', new \ext_dial('${DSTRING}', '${ARG1},${D_OPTIONS}${CWRING}b(func-apply-sipheaders^s^1)'));
+		$ext->add('dialOne-with-exten', '_X','', new \ext_return());
 		/****************************************************************/
 		$ext->add($mcontext,$exten,'', new \ext_execif('$["${DIALSTATUS}"="ANSWER" & "${CALLER_DEST}"!=""]', 'Return'));
 
